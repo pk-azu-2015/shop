7dccc09fd0ad384e616aa3868238e72f091c1d4f
Rafal Chudeusz <rchudeusz@gmail.com>
2015-11-17 15:58:01 +0100

    3rd commit

---
+++ src/Sprzedaz/src/main/java/azu/rchudeusz/shop/WyjscieN.java
@@ -0,0 +1,38 @@
+package azu.rchudeusz.shop;
+
+/**
+ *
+ * @author rchudeusz
+ */
+public class WyjscieN {
+
+    boolean naprawa;
+    boolean zwrot;
+
+    public WyjscieN(boolean naprawa, boolean zwrot) {
+        this.naprawa = naprawa;
+        this.zwrot = zwrot;
+    }
+
+    public boolean isNaprawa() {
+        return naprawa;
+    }
+
+    public void setNaprawa(boolean naprawa) {
+        this.naprawa = naprawa;
+    }
+
+    public boolean isZwrot() {
+        return zwrot;
+    }
+
+    public void setZwrot(boolean zwrot) {
+        this.zwrot = zwrot;
+    }
+
+    @Override
+    public String toString() {
+        return "WyjscieN{" + "naprawa=" + naprawa + ", zwrot=" + zwrot + '}';
+    }
+
+}
---
+++ src/Sprzedaz/src/main/java/azu/rchudeusz/shop/WyjscieS.java
@@ -0,0 +1,19 @@
+package azu.rchudeusz.shop;
+
+import java.util.List;
+
+/**
+ *
+ * @author rchudeusz
+ */
+
+public class WyjscieS {
+    
+    double cena;
+    //List <String> kod_sprzedazy ;
+  //  List <String> kod_produktu ;
+    String kod_sprzedazy;
+    String kod_produktu;
+    List<Pair<String, String>> lista;
+  
+}
