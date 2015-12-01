e6c6a43ae1f69602477b1c73be5f55bef0444207
lkucik <luk.kucik@gmail.com>
2015-12-01 15:21:13 +0100

    Produkt add

--- src/Magazyn/src/main/java/com/mycompany/magazyn_azu/Magazyn.java
+++ src/Magazyn/src/main/java/com/mycompany/magazyn_azu/Magazyn.java
@@ -5,7 +5,12 @@
  */
 package com.mycompany.magazyn_azu;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 import javax.jws.Oneway;
 import javax.jws.WebService;
@@ -19,30 +24,57 @@ import org.w3c.dom.events.EventException;
  */
 @WebService(serviceName = "Magazyn")
 public class Magazyn {
-Logger log;
-    /**
-     * This is a sample web service operation
-     */
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
+
+    private List<Produkt> sprzedaz;
+    private List<Produkt> naprawa;
+    Logger log;
+
+    public List<Produkt> getSprzedaz() {
+        return sprzedaz;
     }
+
+    public List<Produkt> getNaprawa() {
+        return naprawa;
+    }
+
+    public Magazyn() {
+        sprzedaz = new ArrayList<>();
+        naprawa = new ArrayList<>();
+        inicjuj();
+    }
+
+    private void inicjuj() {
+        sprzedaz.add(new Produkt(1, "przedmiot1", "opis_przedmiot1", BigDecimal.valueOf(19.99)));
+        sprzedaz.add(new Produkt(2, "przedmiot2", "opis_przedmiot2", BigDecimal.valueOf(19.99)));
+        sprzedaz.add(new Produkt(3, "przedmiot3", "opis_przedmiot3", BigDecimal.valueOf(19.99)));
+        sprzedaz.add(new Produkt(4, "przedmiot4", "opis_przedmiot4", BigDecimal.valueOf(19.99)));
+        sprzedaz.add(new Produkt(4, "przedmiot5", "opis_przedmiot5", BigDecimal.valueOf(19.99)));
+    }
+
     @Oneway
-   @WebMethod(operationName = "dodajDoMagazynu")
+    @WebMethod(operationName = "dodajDoMagazynu")
     public void dodajDoMagazynu(@WebParam(name = "kod_prod") int kod) {
         
     }
-     @WebMethod(operationName = "pobierz")
+
+    @WebMethod(operationName = "pobierz")
     public int pobierz(@WebParam(name = "name") int kod) {
         return kod;
     }
-     @WebMethod(operationName = "uzupelnij")
-    public void uzupelnij() {     
-       log.info(new Date().toString());
+
+    @WebMethod(operationName = "uzupelnij")
+    public void uzupelnij() {
+        log.info(new Date().toString());
     }
-     @WebMethod(operationName = "wyswietlStan")
-    public int wyswietlStan(@WebParam(name="kod")int kod) throws EventException{
-        int ilosc=0;
+
+    @WebMethod(operationName = "wyswietlStan")
+    public int wyswietlStan(@WebParam(name = "kod") int kod) throws EventException {
+        int ilosc = 0;
+        for(Produkt x: sprzedaz){
+            if(x.getKodProduktu() == kod){
+                ilosc++;
+            }
+        }
         return ilosc;
     }
 }
---
+++ src/Magazyn/src/main/java/com/mycompany/magazyn_azu/Produkt.java
@@ -0,0 +1,55 @@
+package com.mycompany.magazyn_azu;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author nyga
+ */
+public class Produkt {
+
+    private String nazwa;
+    private String opis;
+    private long kodProduktu;
+    private BigDecimal cena;
+
+    public Produkt(long kodProduktu, String nazwa, String opis, BigDecimal cena) {
+        this.nazwa = nazwa;
+        this.kodProduktu = kodProduktu;
+        this.opis = opis;
+        this.cena = cena;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public long getKodProduktu() {
+        return kodProduktu;
+    }
+
+    public void setKodProduktu(long kodProduktu) {
+        this.kodProduktu = kodProduktu;
+    }
+
+    public BigDecimal getCena() {
+        return cena;
+    }
+
+    public void setCena(BigDecimal cena) {
+        this.cena = cena;
+    }
+
+}
