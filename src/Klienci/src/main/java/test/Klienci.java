a790e6a0ffc6d429ed61b00f3ab0dc9b3c35a5b2
kzabicki <konrad.zab@gmail.com>
2015-12-08 15:58:17 +0100

    test

--- src/Klienci/src/main/java/test/Klienci.java
+++ src/Klienci/src/main/java/test/Klienci.java
@@ -8,6 +8,13 @@ package test;
 import javax.jws.Oneway;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  *
@@ -16,23 +23,80 @@ import javax.jws.WebMethod;
 @WebService(serviceName = "Klienci")
 public class Klienci {
 
-    /**
-     * Metoda, ktora dodaje klienta
-     *
-     * @param imie
-     * @param nazwisko
-     * @param adres
-     * @param NIP
-     * @param PESEL
-     * @param nrTel
-     * @return 
-     */
+    java.sql.Connection conn=null;
+    
+       public void polaczenieDoBazyDanych(){
+           System.out.println("Proba nawiazania polaczenia ze sterownikiem");
+           
+           try{
+               Class.forName("com.mysql.jdbc.Driver").newInstance();
+           }
+           
+           catch(ClassNotFoundException | InstantiationException |
+               IllegalAccessException e){
+               
+               
+            System.out.println("Blad przy ladowaniu sterownika bazy!");
+            System.exit(1);
+        }
+        System.out.print("Sterownik OK");
+        
+             System.out.print("Sterownik OK");
+
+        // LACZENIE Z BAZA
+        System.out.print("\nLaczenie z baza danych:");
+        String baza = "jdbc:mysql://db4free.net/azu2015klienci";
+        String user = "kubawozniak";
+        String pass = "bazaazu";
+        java.sql.Connection conn = null;
+        
+        try {
+            conn = DriverManager.getConnection(baza, user, pass);
+        }
+        catch (SQLException e) {
+            System.out.println("Blad przy ladowaniu sterownika bazy!!");
+            System.exit(1);
+        }
+        System.out.print(" polaczenie OK\n");
+
+    
+       }
+    
+    
     @WebMethod(operationName = "dodaj_klienta")
-    public String dodajKlienta(String imie, String nazwisko, 
-                String adres, int NIP, int nrTel, int PESEL) {
-        int ID_klienta = PESEL;
-        return "ID nowego klienta: " + ID_klienta;
+    public void dodajKlienta(@WebParam(name = "imie") String imie, 
+                @WebParam(name = "nazwisko") String nazwisko, 
+                @WebParam(name = "adres") String adres, 
+                @WebParam( name = "NIP" )int NIP, 
+                @WebParam(name = "nrTel")int nrTel, 
+                @WebParam(name = "PESEL")long PESEL) {
+        polaczenieDoBazyDanych();
+        
+        Statement statement = null;
+        
+        try{
+            statement=conn.createStatement();
+            
+            statement.executeUpdate("Insert INTO klienci " + 
+                    "(imie, nazwisko, adres, NIP, nrTel, PESEL) values " 
+                    + "(" + imie + "," 
+                    + nazwisko + ","
+                    + adres + ","
+                    + NIP + ","
+                    + nrTel + ","
+                    + PESEL + ");" );
+            
+        }
+       catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+       }     
+            
+       System.out.println("Dodano klienta do bazy");
+       
+       closeConnection(statement);
+    
     }
+    
     
      /**
      * Metoda, ktora usuwa klienta
@@ -40,9 +104,29 @@ public class Klienci {
      * @param ID_klienta
      */ 
     @Oneway
-    @WebMethod(operationName = "usun_klienta")
-    public void Usun(String ID_klienta) {
+    @WebMethod(operationName = "usun")
+    public void Usun(long PESEL) {
+        
+        polaczenieDoBazyDanych();
+        
+        
+        Statement statement = null;
+        
+        try{
+            statement = conn.createStatement();
+            statement.executeQuery("Delete FROM klienci WHERE PESEL = " + PESEL +";");
+            
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+         System.out.println("Dodano klienta do bazy");
+       
+       closeConnection(statement);
+    
     }
+    
      
     /**
      * Metoda, ktora edytuje klienta
@@ -54,8 +138,11 @@ public class Klienci {
      */
     @Oneway
     @WebMethod(operationName = "edytuj_klienta")
-    public void Edytuj(String imie, String nazwisko, 
-                String adres, int nrTel) {
+    public void Edytuj(@WebParam( name = "imie") String imie, 
+                       @WebParam( name = "nazwisko") String nazwisko,
+                       @WebParam( name = "adres") String adres, 
+                       @WebParam( name="nrTel") int nrTel) {
+        
     }   
     
     /**
@@ -64,9 +151,12 @@ public class Klienci {
      */
     @Oneway
     @WebMethod(operationName = "dodaj_do_historii_zakupow")
-    public void /*List<ListaPlac>*/ dodajDoHistorii() {
-        //List<ListaPlac> lista = new ArrayList<>();
-      //  return lista;
+    public void  dodajDoHistorii(@WebParam(name ="kodSprzedazy")int kodSprzedazy, 
+            @WebParam(name = "PESEL")long PESEL) {
+      
+        
+        
+        
     }
     
     /**
@@ -79,5 +169,23 @@ public class Klienci {
         int rabat = 0;
         //List<ListaPlac> lista = new ArrayList<>();
         return rabat;
+    }
+    
+      private void closeConnection(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 }
