2d013bad642e718bdd25ae9246e69b41220d0e93
zpimak <cooltmp@wp.pl>
2015-12-15 14:28:16 +0100

    Magazyn 15.12.15

--- src/Magazyn/src/main/java/com/mycompany/magazyn_azu/Magazyn.java
+++ src/Magazyn/src/main/java/com/mycompany/magazyn_azu/Magazyn.java
@@ -5,18 +5,19 @@
  */
 package com.mycompany.magazyn_azu;
 
+import com.nyga.azu.asortyment.Asortyment_Service;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 import javax.jws.Oneway;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.xml.ws.WebServiceRef;
 import org.w3c.dom.events.EventException;
+import com.nyga.azu.asortyment.Produkt;
 
 /**
  *
@@ -25,11 +26,18 @@ import org.w3c.dom.events.EventException;
 @WebService(serviceName = "Magazyn")
 public class Magazyn {
 
-    private List<Produkt> sprzedaz;
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/Asortyment/asortyment.wsdl")
+    private Asortyment_Service service;
+
+    // @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/Asortyment/asortyment.wsdl")
+    // private com.nyga.azu.asortyment.Asortyment_Service service;
+    // List<Produkt> sprzedaz;
+    private List<ProduktMg> sprzedaz;
+    // private List<Produkt> sprzedaz;
     private List<Produkt> naprawa;
     Logger log;
 
-    public List<Produkt> getSprzedaz() {
+    public List<ProduktMg> getSprzedaz() {
         return sprzedaz;
     }
 
@@ -44,34 +52,102 @@ public class Magazyn {
     }
 
     private void inicjuj() {
-        sprzedaz.add(new Produkt(1, "przedmiot1", "opis_przedmiot1", BigDecimal.valueOf(19.99)));
-        sprzedaz.add(new Produkt(2, "przedmiot2", "opis_przedmiot2", BigDecimal.valueOf(19.99)));
-        sprzedaz.add(new Produkt(3, "przedmiot3", "opis_przedmiot3", BigDecimal.valueOf(19.99)));
-        sprzedaz.add(new Produkt(4, "przedmiot4", "opis_przedmiot4", BigDecimal.valueOf(19.99)));
-        sprzedaz.add(new Produkt(4, "przedmiot5", "opis_przedmiot5", BigDecimal.valueOf(19.99)));
+        //sprzedaz.add(new Produkt(1, "przedmiot1", "opis_przedmiot1", BigDecimal.valueOf(19.99)));
+        //   sprzedaz.add(new Produkt(2, "przedmiot2", "opis_przedmiot2", BigDecimal.valueOf(19.99)));
+        // sprzedaz.add(new Produkt(3, "przedmiot3", "opis_przedmiot3", BigDecimal.valueOf(19.99)));
+        //sprzedaz.add(new Produkt(4, "przedmiot4", "opis_przedmiot4", BigDecimal.valueOf(19.99)));
+        //sprzedaz.add(new Produkt(4, "przedmiot5", "opis_przedmiot5", BigDecimal.valueOf(19.99)));
     }
 
     @Oneway
     @WebMethod(operationName = "dodajDoMagazynu")
     public void dodajDoMagazynu(@WebParam(name = "kod_prod") int kod) {
-        
+
+        try { // Call Web Service Operation
+            com.nyga.azu.asortyment.Asortyment port = service.getAsortymentPort();
+            // TODO process result here
+            java.util.List<com.nyga.azu.asortyment.Produkt> result = port.pobierzListe();
+            //  System.out.println("Result = "+result);
+            int i = 0;
+            boolean existAso = false;
+            boolean existMg = false;
+
+            for (Produkt x : result) {
+                if (x.getKodProduktu() == kod) {
+                    existAso = true;
+                }
+            }
+
+            for (ProduktMg y : sprzedaz) {
+                if (y.getKodProduktu() == kod) {
+                    existMg = true;
+                }
+            }
+            if (existAso == false && existMg == false) {
+                sprzedaz.add(new ProduktMg(kod, 1));
+            }
+
+            //   sprzedaz = new ArrayList<>(result);
+            System.out.println("Result sprzedaz = " + sprzedaz);
+
+        } catch (Exception ex) {
+            // TODO handle custom exceptions here
+        }
+
     }
 
     @WebMethod(operationName = "pobierz")
-    public int pobierz(@WebParam(name = "name") int kod) {
-        return kod;
+    public int pobierz(@WebParam(name = "kod") int kod) {
+     long ilosc=0;
+       
+                for (ProduktMg y : sprzedaz) {
+                    if (y.getKodProduktu() == kod) {
+                        y.setIlosc(y.getIlosc() -1);
+                       ilosc = y.getIlosc();
+                    }
+                }
+  
+            System.out.println("Result sprzedaz = " + sprzedaz);
+
+      
+        
+        
+       return (int)ilosc; 
     }
 
     @WebMethod(operationName = "uzupelnij")
     public void uzupelnij() {
-        log.info(new Date().toString());
+
+        try { // Call Web Service Operation
+            com.nyga.azu.asortyment.Asortyment port = service.getAsortymentPort();
+            // TODO process result here
+            java.util.List<com.nyga.azu.asortyment.Produkt> result = port.pobierzListe();
+            //  System.out.println("Result = "+result);
+            int i = 0;
+            int ilosc;
+            for (Produkt x : result) {
+                i++;
+                for (ProduktMg y : sprzedaz) {
+                    if (x.getKodProduktu() == y.getKodProduktu()) {
+                        sprzedaz.get(i).setIlosc(y.getIlosc() + 1);
+                    }
+                }
+            }
+
+            //   sprzedaz = new ArrayList<>(result);
+            System.out.println("Result sprzedaz = " + sprzedaz);
+
+        } catch (Exception ex) {
+            // TODO handle custom exceptions here
+        }
+        // log.info(new Date().toString());
     }
 
     @WebMethod(operationName = "wyswietlStan")
     public int wyswietlStan(@WebParam(name = "kod") int kod) throws EventException {
         int ilosc = 0;
-        for(Produkt x: sprzedaz){
-            if(x.getKodProduktu() == kod){
+        for (ProduktMg x : sprzedaz) {
+            if (x.getKodProduktu() == kod) {
                 ilosc++;
             }
         }
